b0d720f456f69a7d3e486fc8f2796eb3b9b4067f
PDanowski <piter1403@gmail.com>
2022-07-19 22:36:01 +0200

    Angular updated to 11 part 2

--- TestCreator/TestCreator.WebApp/ClientApp/src/tests/services/test.result.service.spec.ts
+++ TestCreator/TestCreator.WebApp/ClientApp/src/tests/services/test.result.service.spec.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { async, TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TestResultService } from 'src/app/services/test.result.service';
 
